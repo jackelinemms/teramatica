95da9b151a677f813c9b5333f2e5a4d345c05b95
jackelinemms <jackeline.martinst@gmail.com>
2023-09-25 00:41:00 -0300

    arrumando alguns erros de jsx

--- src/components/pages/Perfil.jsx
+++ src/components/pages/Perfil.jsx
@@ -10,17 +10,17 @@ export default function Perfil() {
         <h1>Fulano de Tal</h1>
         <div className="field-margin-top">
           {/* email */}
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <label forHtml="email" className="col-sm-2 col-form-label">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <label htmlFor="email" className="col-sm-2 col-form-label">
                   Email
                 </label>
               </div>
-              <div class="col">
+              <div className="col">
                 <input type="email" className="form-control" id="email"></input>
               </div>
-              <div class="col">
+              <div className="col">
                 <button type="button" className="btn btn-primary btn-editar">
                   Editar
                 </button>
@@ -31,23 +31,23 @@ export default function Perfil() {
 
         {/* senha */}
         <div className="field-margin-top">
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <label forHtml="password" className="col-sm-2 col-form-label">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <label htmlFor="password" className="col-sm-2 col-form-label">
                   Senha
                 </label>
               </div>
-              <div class="col">
+              <div className="col">
                 <input
                   type="password"
                   className="form-control"
                   id="password"
                 ></input>
               </div>
-              <div class="col">
+              <div className="col">
                 <button type="button" className="btn btn-primary btn-editar">
-                  Editar
+                  <img src={edit} alt="editar" />
                 </button>
               </div>
             </div>
